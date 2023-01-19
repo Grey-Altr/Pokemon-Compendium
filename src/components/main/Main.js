f947535dbd1873f85b77cc0bfb9c075059276395
Grey Altr <grey.sifr@gmail.com>
2023-01-19 15:15:56 -0800

    types dropdown displays on page

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { usePokemon } from '../../hooks/usePokemon.js';
+import Select from './controls/select/Select.js';
 
 export default function Main() {
   const { pokemon, types } = usePokemon();
 
   return (
-    <div>
-      <h1>Pokemon Compendium!</h1>
-      {/* controls */}
-      {/* cards */}
-      <section>
+    <>
+      <section className="controls">
+        <h1>Pokemon Compendium!</h1>
+        <Select types={types} />
+      </section>
+      <section className="cards">
         {pokemon.map((poke) => (
           <p key={poke._id}>
             {poke.pokemon} {poke.type_1} {poke.type_2}
           </p>
         ))}
       </section>
-    </div>
+    </>
   );
 }
